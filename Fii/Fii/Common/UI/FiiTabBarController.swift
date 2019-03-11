49562471e8674532ebeffc1b53eabed2d9a92281
xiaolangshou <xiaolangshou@foxmail.com>
2019-03-11 20:02:10 +0800

    add FiiTababr

---
+++ Fii/Fii/Common/UI/FiiTabBarController.swift
@@ -0,0 +1,32 @@
+//
+//  FiiTabBarController.swift
+//  Fii
+//
+//  Created by Liu Tao on 2019/3/11.
+//  Copyright © 2019年 Liu Tao. All rights reserved.
+//
+
+import UIKit
+
+class FiiTabBarController: UITabBarController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
+
+class FiiTabBar: UITabBar {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        self.barTintColor = UIColor.white
+        self.isTranslucent = false
+        
+        let normal_attributes =[]
+    }
+}
