2e27057baff93d96e5caf2d8f72dd80be99d2950
xiaolangshou <xiaolangshou@foxmail.com>
2019-03-12 09:42:38 +0800

    add Tabbar VC

--- Fii/Fii/AppMain/AppDelegate.swift
+++ Fii/Fii/AppMain/AppDelegate.swift
@@ -13,17 +13,68 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
+    {
+        let vc1 = HomeVC()
+        let vc2 = MeVC()
+        let vc3 = DeviceListVC()
+        let vc4 = VoiceControllVC()
+        let vc5 = RealTimeVC()
+        
+        let childVCs = [
+            createTabbarChildVC(vc: vc1,
+                                title: "首页",
+                                normalImage: UIImage("home")!,
+                                selectImage: UIImage("home")!,
+                                tag: 0),
+            createTabbarChildVC(vc: vc2,
+                                title: "实时快照",
+                                normalImage: UIImage("screen_shot")!,
+                                selectImage: UIImage("screen_shot")!,
+                                tag: 0),
+            createTabbarChildVC(vc: vc3,
+                                title: "语音",
+                                normalImage: UIImage("voice")!,
+                                selectImage: UIImage("voice")!,
+                                tag: 0),
+            createTabbarChildVC(vc: vc4,
+                                title: "设备数据",
+                                normalImage: UIImage("device_data")!,
+                                selectImage: UIImage("device_data")!,
+                                tag: 0),
+            createTabbarChildVC(vc: vc5, title: "我的",
+                                normalImage: UIImage.init("me")!,
+                                selectImage: UIImage.init("me")!,
+                                tag: 0)
+        ]
+        
+        let rootVC = FiiTabBarController(childVCs)
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.backgroundColor = UIColor.white
+        window?.rootViewController = rootVC
         window?.makeKeyAndVisible()
-        window?.rootViewController = UINavigationController(rootViewController: MainViewController.shared)
         
         // 开屏图停留
         _ = Thread.sleep(forTimeInterval: 1.0)
         
         return true
+    }
+    
+    func createTabbarChildVC(vc: UIViewController,
+                             title: String,
+                             normalImage: UIImage,
+                             selectImage: UIImage,
+                             tag: Int) -> UIViewController
+    {
+        let item = UITabBarItem.init(title: title,
+                                     image: normalImage.withRenderingMode(.alwaysOriginal),
+                                     tag: tag)
+        item.selectedImage = selectImage.withRenderingMode(.alwaysOriginal)
+        vc.tabBarItem = item
+        
+        return UINavigationController(rootViewController: vc)
     }
 
 }
--- Fii/Fii/Common/UI/FiiTabBarController.swift
+++ Fii/Fii/Common/UI/FiiTabBarController.swift
@@ -10,13 +10,25 @@ import UIKit
 
 class FiiTabBarController: UITabBarController {
 
+    convenience init(_ subVCs: [UIViewController]) {
+        self.init(nibName: nil, bundle: nil)
+        
+        subVCs.forEach { vc in
+            self.addChild(vc)
+        }
+    }
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
+    override func loadView() {
+        super.loadView()
+        
+        let tabBar = FiiTabBar(frame: CGRect.zero)
+        self.setValue(tabBar, forKey: "tabBar")
     }
 
+    override var childForStatusBarStyle: UIViewController? {
+        
+        return self.selectedViewController
+    }
 }
 
 class FiiTabBar: UITabBar {
@@ -27,6 +39,16 @@ class FiiTabBar: UITabBar {
         self.barTintColor = UIColor.white
         self.isTranslucent = false
         
-        let normal_attributes =[]
+        let normal_attributes = [NSAttributedString.Key.font: UIFont.Kmedium(11),
+                                 NSAttributedString.Key.foregroundColor: UIColor.hex(0x515151)]
+        let select_attributes = [NSAttributedString.Key.font: UIFont.Kmedium(11),
+                                 NSAttributedString.Key.foregroundColor: UIColor.hex(0xdf3231)]
+        
+        UITabBarItem.appearance().setTitleTextAttributes(normal_attributes, for: .normal)
+        UITabBarItem.appearance().setTitleTextAttributes(select_attributes, for: .selected)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
