66f7ec7fbd0c964951c71ea7707a5848a1a1b0c2
xiaolangshou <xiaolangshou@foxmail.com>
2019-03-12 14:27:18 +0800

    "test"

---
+++ Fii/Fii/Business/Login/View/FiiPlayerView.swift
@@ -0,0 +1,22 @@
+//
+//  FiiPlayerView.swift
+//  Fii
+//
+//  Created by Liu Tao on 2019/3/12.
+//  Copyright © 2019年 Liu Tao. All rights reserved.
+//
+
+import UIKit
+
+class FiiPlayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+     // 
+    }
+    */
+
+}
