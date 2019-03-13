eb5b25818067bb515b2f20caa99eff7c1ea2796e
xiaolangshou <xiaolangshou@foxmail.com>
2019-03-13 22:01:47 +0800

    update open screen view

--- Fii/Fii/Business/Login/View/OpenScreenView.swift
+++ Fii/Fii/Business/Login/View/OpenScreenView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Then
 import SnapKit
 
 class OpenScreenView: UIView {
@@ -22,6 +23,14 @@ class OpenScreenView: UIView {
     
     let imgArr = ["bg1", "bg2", "bg3", "bg4"]
     let circleImgArr = ["bg11", "bg22", "bg33", "bg44"]
+    let titleArr = ["Foxconn Industry Internet",
+                    "A Fully Automated Workshop",
+                    "Industrial Big Data Platform",
+                    "Artificial Intelligence"]
+    let subTitleArr = ["全球领先的工业互联网智能制造和科技服务解决方案",
+                       "以无人化、无纸化、影像全纪录来贯彻智能化的运作，实现自主的关灯作业工厂",
+                       "上线、云端、联网、互通、反馈的智慧数据平台",
+                       "视觉 + 语音 + 判断 + 预测 重新型塑未来制造产业"]
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -57,14 +66,53 @@ class OpenScreenView: UIView {
         scrollView.delegate = self
         scrollView.contentSize = CGSize(width: width * CGFloat(count), height: height)
         
-        pageControl.center.x = self.width / 2
-        pageControl.frame.origin.y = self.height - 10 + pageControl.height
+        pageControl.center.x = width / 2
+        pageControl.frame.origin.y = height - 10 + pageControl.height
         pageControl.numberOfPages = count
         
         for i in 0..<count {
-            let imgView = UIImageView.init(frame: CGRect.init(CGFloat(i) * width , 0, width, height))
-            imgView.added(into: scrollView)
-            imgView.image = UIImage.init(imgArr[i])
+            
+            _ = UIImageView().then { (imgV) in
+                imgV.frame = CGRect(CGFloat(i) * width,
+                                    0,
+                                    width,
+                                    height)
+                imgV.added(into: scrollView)
+                imgV.image = UIImage(imgArr[i])
+                
+                _ = UIImageView().then({ (subImgV) in
+                    subImgV.frame = CGRect(0.2 * width,
+                                           0.3 * width,
+                                           0.6 * width,
+                                           0.6 * width)
+                    subImgV.added(into: imgV)
+                    subImgV.image = UIImage(circleImgArr[i])
+                })
+                
+                let titleLbl = UILabel().then({ (title) in
+                    title.Text(titleArr[i]).TextAlignment(.center).TextColor(UIColor.white).Font(.Kmedium(26))
+                    title.added(into: imgV)
+                    title.snp.makeConstraints({ (make) in
+                        make.width.equalToSuperview()
+                        make.height.equalTo(40)
+                        make.top.equalTo(width)
+                        make.centerX.equalToSuperview()
+                    })
+                })
+                
+                _ = UILabel().then({ (subT) in
+                    subT.numberOfLines = 0
+                    subT.lineBreakMode = .byWordWrapping
+                    subT.Text(subTitleArr[i]).TextAlignment(.center).TextColor(UIColor.white).Font(.Kmedium(15))
+                    subT.added(into: imgV)
+                    subT.snp.makeConstraints({ (make) in
+                        make.width.equalTo(width * 0.8)
+                        make.height.equalTo(80)
+                        make.top.equalTo(titleLbl.snp.bottom).offset(5)
+                        make.centerX.equalToSuperview()
+                    })
+                })
+            }
         }
     }
 }
