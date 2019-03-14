49c691b4f3559727038db95af58d9f7149bf8818
xiaolangshou <xiaolangshou@foxmail.com>
2019-03-14 14:38:39 +0800

    update me vc

--- Fii/Fii/Business/Me/ViewController/MeVC.swift
+++ Fii/Fii/Business/Me/ViewController/MeVC.swift
@@ -16,7 +16,7 @@ class MeVC: UIViewController {
                    (img: UIImage("help"), name: "帮助"),
                    (img: UIImage("customer_service"), name: "客服"),
                    (img: UIImage("like"), name: "收藏"),
-                   (img: UIImage("settings"), name: "配置")]
+                   (img: UIImage("settings"), name: "设置")]
     
     let scrollView = UIScrollView()
     let containerView = UIView()
@@ -49,28 +49,51 @@ class MeVC: UIViewController {
     func setupCells() {
         
         let topView = UIImageView().then({ (v) in
+            v.ImageName("me_back")
             v.isUserInteractionEnabled = true
-            v.added(into: containerView)
+            v.added(into: scrollView)
             v.snp.makeConstraints({ (make) in
                 make.left.right.top.equalToSuperview()
                 make.height.equalTo(150)
             })
             
-            _ = UIView().then({ (line) in
-                line.backgroundColor = UIColor.gray
-                line.added(into: v)
-                line.snp.makeConstraints({ (make) in
+            let avatar = UIImageView().then({ (imgV) in
+                imgV.added(into: v)
+                imgV.backgroundColor = UIColor.green
+                imgV.cornerRadius = 30
+                imgV.snp.makeConstraints({ (make) in
+                    make.width.height.equalTo(60)
+                    make.centerY.equalToSuperview()
                     make.left.equalTo(30)
-                    make.right.equalTo(-30)
-                    make.bottom.equalToSuperview()
-                    make.height.equalTo(0.5)
                 })
+            })
+            
+            _ = UILabel().then({ (lbl) in
+                lbl.added(into: v)
+                lbl.snp.makeConstraints({ (make) in
+                    make.left.equalTo(avatar.snp.right).offset(10)
+                    make.right.equalTo(-50)
+                    make.height.equalTo(20)
+                    make.top.equalTo(avatar.snp.top).offset(8)
+                })
+                lbl.Text("Thomas Lau").TextColor(UIColor.white).Font(UIFont.MILanTing(15))
+            })
+
+            _ = UILabel().then({ (lbl) in
+                lbl.added(into: v)
+                lbl.snp.makeConstraints({ (make) in
+                    make.left.equalTo(avatar.snp.right).offset(10)
+                    make.right.equalTo(-50)
+                    make.height.equalTo(20)
+                    make.bottom.equalTo(avatar.snp.bottom).offset(-8)
+                })
+                lbl.Text("xiaolangshou@foxmail.com").TextColor(UIColor.white).Font(UIFont.MILanTing(14))
             })
         })
         
         let quitView = UIButton().then { (v) in
             v.backgroundColor = UIColor.white
-            v.added(into: containerView)
+            v.added(into: scrollView)
             v.snp.makeConstraints({ (make) in
                 make.left.right.equalToSuperview()
                 make.height.equalTo(50)
@@ -83,7 +106,7 @@ class MeVC: UIViewController {
         var prevCell: Cell?
         for (index, item) in dataArr.enumerated() {
             let cell = Cell()
-            cell.added(into: containerView)
+            cell.added(into: scrollView)
             cell.image = item.img
             cell.title = item.name
             
@@ -148,6 +171,12 @@ class Cell: UIView {
         
         backgroundColor = UIColor.white
         
+        let tapGesture = UITapGestureRecognizer.init()
+        tapGesture.numberOfTapsRequired = 1
+        tapGesture.addTarget(self, action: #selector(tapGestureBtnTapped))
+        
+        self.addGestureRecognizer(tapGesture)
+        
         imageV.added(into: self)
         imageV.snp.makeConstraints { (make) in
             make.width.height.equalTo(20)
@@ -171,5 +200,9 @@ class Cell: UIView {
             make.height.equalTo(50)
             make.centerY.equalToSuperview()
         }
+    }
+    
+    @objc func tapGestureBtnTapped() {
+        print("tapGestureBtnTapped...")
     }
 }
