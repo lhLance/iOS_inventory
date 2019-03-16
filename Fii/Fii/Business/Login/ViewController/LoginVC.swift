41e388a2040c85427e3f71cb348c260367d22c92
xiaolangshou <xiaolangshou@foxmail.com>
2019-03-16 10:03:07 +0800

    update login vc

--- Fii/Fii/Business/Login/ViewController/LoginVC.swift
+++ Fii/Fii/Business/Login/ViewController/LoginVC.swift
@@ -9,23 +9,113 @@
 import UIKit
 import SnapKit
 import Then
+import RxSwift
+import RxCocoa
 
 class LoginVC: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        setupView()
-    }
+    let logInLbl = UILabel()
+    let userNameTf = UITextField()
+    let passwordTf = UITextField()
+    let loginBtn = UIButton()
+    let regisBtn = UIButton()
+    let bag = DisposeBag()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        self.navigationController?.isNavigationBarHidden = false
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupView()
+        bindUI()
     }
 
     func setupView() {
         
-        view.backgroundColor = UIColor.green
+        view.backgroundColor = UIColor.white
+        
+        logInLbl.TextColor(UIColor.black).TextFont("登录", UIFont.MILanTing(16))
+        logInLbl.added(into: view)
+        logInLbl.snp.makeConstraints { (make) in
+            make.left.equalTo(15)
+            make.top.equalTo(UIScreen.navBarHeight + 30)
+            make.width.equalTo(60)
+            make.height.equalTo(25)
+        }
+        
+        userNameTf.borderStyle = .line
+        userNameTf.keyboardType = .phonePad
+        userNameTf.attributedPlaceholder = NSAttributedString(string: "请输入账号",
+                                                              attributes: [NSAttributedString.Key.font : UIFont.MILanTing(14), NSAttributedString.Key.foregroundColor: UIColor.hex(0x9a9a9a)])
+        userNameTf.added(into: view)
+        userNameTf.snp.makeConstraints { (make) in
+            make.left.equalTo(15)
+            make.right.equalTo(-15)
+            make.height.equalTo(20)
+            make.top.equalTo(logInLbl.snp.bottom).offset(5)
+        }
+        
+        passwordTf.borderStyle = .line
+        passwordTf.keyboardType = .phonePad
+        passwordTf.attributedPlaceholder = NSAttributedString(string: "请输入密码",
+                                                              attributes: [NSAttributedString.Key.font : UIFont.MILanTing(14), NSAttributedString.Key.foregroundColor: UIColor.hex(0x9a9a9a)])
+        passwordTf.added(into: view)
+        passwordTf.snp.makeConstraints { (make) in
+            make.left.equalTo(15)
+            make.right.equalTo(-15)
+            make.height.equalTo(20)
+            make.top.equalTo(userNameTf.snp.bottom).offset(20)
+        }
+        
+        loginBtn.cornerRadius = 4
+        loginBtn.backgroundColor = UIColor.hex(0x9DCCFF)
+        loginBtn.Text("登录").Font(UIFont.MILanTing(16)).TitleColor(UIColor.white)
+        loginBtn.added(into: view)
+        loginBtn.snp.makeConstraints { (make) in
+            make.left.equalTo(15)
+            make.right.equalTo(-15)
+            make.height.equalTo(50)
+            make.top.equalTo(passwordTf.snp.bottom).offset(50)
+        }
+        loginBtn.addTarget(self, action: #selector(loginBtnTapped), for: .touchUpInside)
+        
+        regisBtn.Text("注册新账号").TitleColor(UIColor.hex(0x9DCCFF)).Font(UIFont.MILanTing(14))
+        regisBtn.added(into: view)
+        regisBtn.snp.makeConstraints { (make) in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(loginBtn.snp.bottom).offset(10)
+        }
+        regisBtn.addTarget(self, action: #selector(registerBtnTapped), for: .touchUpInside)
     }
+    
+    @objc func loginBtnTapped() {
+        print("loginBtnTapped...")
+    }
+    
+    @objc func registerBtnTapped() {
+        let vc = RegisterVC()
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    func bindUI() {
+        
+        Observable.combineLatest(userNameTf.rx.text.orEmpty,
+                                 passwordTf.rx.text.orEmpty) { [weak self] phoneNumber, password in
+                                    if phoneNumber.count > 0
+                                        && password.count > 0
+                                    {
+                                        self?.loginBtn.isEnabled = true
+                                        self?.loginBtn.backgroundColor = UIColor.hex(0x2C7CFD)
+                                    } else {
+                                        self?.loginBtn.isEnabled = false
+                                        self?.loginBtn.backgroundColor = UIColor.hex(0x9DCCFF)
+                                    }
+            }.subscribe().disposed(by: bag)
+        
+    }
+    
 }
